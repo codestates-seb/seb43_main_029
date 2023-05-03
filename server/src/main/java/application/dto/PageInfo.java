72340fa225af694a7a3f1660db0b6dbc9cb72263
SEBBE43JUN <eguana111@gmail.com>
2023-05-03 15:59:37 +0900

    dto

--- server/src/main/java/application/dto/PageInfo.java
+++ server/src/main/java/application/dto/PageInfo.java
@@ -1,14 +1,22 @@
 package application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 public class PageInfo {
-    private int currentPage;
-    private int pageSize;
+    //현재 페이지 번호 저장
+    private int page;
+    //페이지당 보여줄 요소의 개수 저장
+    private int size;
+    //전체 페이지의 개수 저장
     private int totalPages;
-
-    public PageInfo(int currentPage, int pageSize, int totalPages) {
-        this.currentPage = currentPage;
-        this.pageSize = pageSize;
-        this.totalPages = totalPages;
-    }
-
+    //전체 요소의 개수 저장
+    private long totalElements;
+    //전체 데이터의 개수 저장
 }
--- server/src/main/java/application/dto/SingleResponseDto.java
+++ server/src/main/java/application/dto/SingleResponseDto.java
@@ -1,10 +1,15 @@
 package application.dto;
 
-public class SingleResponseDto<T> {
-    private T data;
 
+import lombok.Getter;
+
+@Getter
+public class SingleResponseDto <T>{
+    private T data; // 단일 데이터를 나타내는 멤버 변수
+
+
+    // 생성자
     public SingleResponseDto(T data) {
-        this.data = data;
+        this.data = data; // 멤버 변수에 데이터 할당
     }
-
 }
