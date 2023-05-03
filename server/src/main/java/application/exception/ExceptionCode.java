adaa8270a91f22a867e4dc1e7bb48ad5f7fdd6ff
YunGeunSang <120304078+YunGeunSang@users.noreply.github.com>
2023-05-03 15:00:55 +0900

    feat: 공용 Exception 클래스 추가

---
+++ server/src/main/java/application/exception/ExceptionCode.java
@@ -0,0 +1,24 @@
+package application.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    /*
+    TODO: 예외 코드 필요 시 상황에 맞게 추가할 것.
+      - 401 Unauthorized, 403 Forbidden, 404 Not Found
+      - 500 Internal Server Error, 502 Bad Gateway
+    */
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_EXISTS(409, "Member exists");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
