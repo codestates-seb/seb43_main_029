ae4af0e81faa8720f563ca49ec4410744d51222b
YunGeunSang <120304078+YunGeunSang@users.noreply.github.com>
2023-05-03 22:46:15 +0900

    feat: 배포 테스트를 위한 회원가입 추가

---
+++ server/src/main/java/application/member/controller/MemberController.java
@@ -0,0 +1,38 @@
+package application.member.controller;
+
+import application.member.dto.MemberDto;
+import application.member.entity.Member;
+import application.member.mapper.MemberMapper;
+import application.member.service.MemberService;
+import application.utils.UriCreator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberMapper memberMapper;
+    private final MemberService memberService;
+
+    public MemberController(MemberMapper memberMapper, MemberService memberService) {
+        this.memberMapper = memberMapper;
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public ResponseEntity postMember(@RequestBody MemberDto.Post requestBody){
+        Member member = memberMapper.memberPostDtoToMember(requestBody);
+
+        Member createdMember = memberService.createMember(member);
+        URI location = UriCreator.createUri("/members", createdMember.getMemberId());
+
+        return ResponseEntity.created(location).build();
+    }
+
+}
---
+++ server/src/main/java/application/member/entity/Member.java
@@ -0,0 +1,58 @@
+package application.member.entity;
+
+import application.audit.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Member extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long memberId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String phone;
+
+    // TODO: 시큐리티 추가시 구현
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    private List<String> roles = new ArrayList<>();
+
+    @Column(length = 11)
+    private String companyNumber;
+
+    @Enumerated(value = EnumType.STRING)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+
+    public enum MemberStatus{
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
+
+}
