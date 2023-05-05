b77e1e2da2f9d86f29b103ffe96b958cc09dc1bb
sinyaenok <dpshrdl56@gmail.com>
2023-05-05 23:44:21 +0900

    feat:Main 컴포넌트 생성

---
+++ client/src/Main/index.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Main = () => {
+  const StyledMain = styled.div`
+    background-color: tomato;
+    width: 100%;
+  `;
+  return <StyledMain>메인페이지</StyledMain>;
+};
+
+export default Main;
