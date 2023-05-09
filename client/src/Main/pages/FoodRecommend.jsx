525ee7cffd1cea6df7729e5ece7fc9f65c3de034
sinyaenok <dpshrdl56@gmail.com>
2023-05-09 23:26:58 +0900

    feat:eslint import/resolver:typescript

--- client/src/Main/pages/FoodRecommend.jsx
+++ client/src/Main/pages/FoodRecommend.jsx
@@ -1,27 +1,49 @@
-import styled from 'styled-components';
-import FoodRecommendBtn from '../component/FoodRecommendBtn';
+// import styled from 'styled-components';
+import './styles.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+
 const M_FoodRecommend = () => {
   return (
-    <M_FoodRecommendContainer>
-      <RecommendTitle>오늘 뭐먹지?</RecommendTitle>
-      <FoodRecommendBtn />
-    </M_FoodRecommendContainer>
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
   );
 };
 
 export default M_FoodRecommend;
 
 //style
-const M_FoodRecommendContainer = styled.section`
-  background-color: tomato;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const RecommendTitle = styled.div`
-  border: 1px solid #000;
-`;
+// const M_FoodRecommendContainer = styled.section`
+//   background-color: tomato;
+//   width: 100vw;
+//   height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `;
