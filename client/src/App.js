3577a8009009db9b3b9da6f197117635d7c47eb0
Latada <icom92@naver.com>
2023-05-09 22:37:11 +0900

    feat: 라우터 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
-import MyPage from './pages/myPage/MyPage.jsx';
+import MyPage from './pages/myPage/MyPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './Main/index';
+import Search from './pages/search/Search';
+import Restaurant from './pages/restaurant/Restaurant';
+import Registration from './pages/registration/Registration';
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
@@ -14,7 +18,11 @@ function App() {
         <GlobalStyle />
         <div className="App"></div>
         <Routes>
+          <Route path="/" element={<Main />} />
           <Route path="/mypage/:id" element={<MyPage />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/restaurant" element={<Restaurant />} />
+          <Route path="/registration" element={<Registration />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/pages/registration/Registration.jsx
@@ -0,0 +1,3 @@
+function Registration() {}
+
+export default Registration;
