1236f3906501303a950f4984c39c5481a56b2094
sinyaenok <dpshrdl56@gmail.com>
2023-05-11 01:56:51 +0900

    design:main/restaurant styledComponent 완성

--- client/src/pages/Main/component/Restaurant.jsx
+++ client/src/pages/Main/component/Restaurant.jsx
@@ -4,16 +4,16 @@ import styled from 'styled-components';
 const Restaurant = props => {
   return (
     <R_Container>
-      <R_Image>
+      <div className="R_Image">
         <img src={props.images} alt={props.name} />
-      </R_Image>
-      <R_Info>
-        <R_NameAndScore>
+      </div>
+      <div className="R_Info">
+        <div className="R_NameAndScore">
           <span className="R_Name">{props.name}</span>
           <span className="R_Score">{props.score}</span>
-        </R_NameAndScore>
-        <R_Address>{props.address}</R_Address>
-      </R_Info>
+        </div>
+        <span className="R_Address">{props.address}</span>
+      </div>
     </R_Container>
   );
 };
@@ -27,39 +27,38 @@ export const R_Container = styled.section`
   height: 250px;
   display: flex;
   flex-direction: column;
-`;
 
-/** 식당 이미지 컴포넌트 */
-const R_Image = styled.div`
-  height: 200px;
-  img {
-    border-radius: 2px 2px 0 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    // 이미지가 뭉개지는 것을 방지
+  //식당 이미지
+  .R_Image {
+    height: 200px;
+    img {
+      border-radius: 2px 2px 0 0;
+      width: 100%;
+      height: 100%;
+      // 이미지가 뭉개지는 것을 방지
+      object-fit: cover;
+    }
   }
-`;
-/**  이름, 별점, 주소 감싸는 역할  */
-const R_Info = styled.div`
-  background-color: #fff;
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  padding: 0.5em;
-  align-items: center;
-`;
 
-const R_NameAndScore = styled.div`
-  font-weight: 700;
-  font-size: 1.2em;
+  .R_Info {
+    background-color: #fff;
+    display: flex;
+    flex-direction: column;
+    padding: 0.5em;
+    align-items: center;
+  }
+  .R_NameAndScore {
+    font-weight: 700;
+    font-size: 1.2em;
+    margin-bottom: 0.2em;
+  }
   .R_Score {
     padding-left: 0.5em;
     color: #ec4899;
   }
-`;
-const R_Address = styled.div`
-  font-size: 0.8em;
-  color: #6b7280;
-  padding-left: 0.2em;
+  .R_Address {
+    font-size: 0.8em;
+    color: #6b7280;
+    padding-left: 0.2em;
+  }
 `;
