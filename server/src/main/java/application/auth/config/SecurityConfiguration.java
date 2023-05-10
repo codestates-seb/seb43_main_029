93f6ac6d99f89fad78e8bee3ce7ea058dddfc03e
YunGeunSang <120304078+YunGeunSang@users.noreply.github.com>
2023-05-10 13:18:35 +0900

    feat: 시큐리티 CORS 설정 배포 환경에 맞게 추가

--- server/src/main/java/application/auth/config/SecurityConfiguration.java
+++ server/src/main/java/application/auth/config/SecurityConfiguration.java
@@ -55,8 +55,16 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        // CORS 요청에서 허용되는 origin을 설정하는 메서드, 모든 origin으로 부터의 요청을 허용한다
+        // S3에서 정적 배포하는 경로가 origin이 된다
+        configuration.setAllowedOrigins(Arrays.asList("http://seb43-main-029-client.s3-website.ap-northeast-2.amazonaws.com"));
+        // 허용되는 HTTP Method 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+        // 허용되는 HTTP 요청 헤더 설정
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        // 클라이언트가 접근할 수 있는 헤더 설정
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
