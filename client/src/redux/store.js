db08a9a31a7dd8ded5df0578f88d37de2774e5c2
sinyaenok <dpshrdl56@gmail.com>
2023-05-18 04:51:40 +0900

    feat:npm i logger

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import restaurantsReducer from './reducers';
+const middleware = [logger, thunk];
 
-export const store = createStore(restaurantsReducer, applyMiddleware(thunk));
+export const store = createStore(restaurantsReducer, applyMiddleware(...middleware));
 
 export default store;
