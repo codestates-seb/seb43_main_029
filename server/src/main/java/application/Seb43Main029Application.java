cd0dcae6fd8ec635318e81850d2082877f1b1baf
SEBBE43JUN <eguana111@gmail.com>
2023-05-18 10:07:37 +0900

    05.18

--- server/src/main/java/application/Seb43Main029Application.java
+++ server/src/main/java/application/Seb43Main029Application.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableScheduling     // 스케쥴러 기능 활성화
-@EnableBatchProcessing  // 배치 기능 활성화
 public class Seb43Main029Application {
 
-	public static void main(String[] args) {SpringApplication.run(Seb43Main029Application.class, args);
-
+	public static void main(String[] args) {
+		SpringApplication.run(Seb43Main029Application.class, args);
 	}
 
 }
