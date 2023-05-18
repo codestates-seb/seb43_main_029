d1a18e5d98884049fe70410f9be67ca50512b66b
sinyaenok <dpshrdl56@gmail.com>
2023-05-18 23:50:08 +0900

    feat:main/category 랜덤카테고리 redux

--- client/src/pages/Main/component/Review/Review_Title.jsx
+++ client/src/pages/Main/component/Review/Review_Title.jsx
@@ -6,8 +6,13 @@ import styled from 'styled-components';
 const Review_Title = () => {
   return (
     <ReviewTitleContainer>
-      <HotReviewTitle>푸피 감수성 MAX 많은 공감을 받는 리뷰</HotReviewTitle>
-      <HotReviewBox>핫 리뷰</HotReviewBox>
+      <HotReviewTitle>
+        <div className="subTitle">푸피 감수성 MAX</div>
+        <div className="Title">많은 공감을 받는 리뷰</div>
+      </HotReviewTitle>
+      <HotReviewBox>
+        <HotReview>여기 요새 왤캐 폼 미츴냐이</HotReview>
+      </HotReviewBox>
       <MemberReviewTitle>회원님들의 식당리뷰</MemberReviewTitle>{' '}
     </ReviewTitleContainer>
   );
@@ -28,21 +33,33 @@ const HotReviewTitle = styled.h1`
   font-weight: 700;
   font-size: 2.2rem;
 
-  flex: 1;
   display: flex;
-  align-items: end;
-
-  padding-bottom: 10px;
+  flex: 1;
+  flex-direction: column;
+  justify-content: end;
 `;
 const HotReviewBox = styled.section`
-  width: 100%;
-
   flex: 3;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const HotReview = styled.div`
+  background: url('https://images.unsplash.com/photo-1494859802809-d069c3b71a8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80')
+    no-repeat center;
+  background-size: cover;
+  color: gray;
+
+  font-weight: 700;
+  font-size: 1.5rem;
+  width: 700px;
+  height: 90%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const MemberReviewTitle = styled.h1`
   font-weight: 700;
   font-size: 1.5rem;
