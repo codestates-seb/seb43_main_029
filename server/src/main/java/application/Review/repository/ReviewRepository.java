b0d4d7c2f88eccaf353a71be722b9da34e399ef9
SEBBE43JUN <eguana111@gmail.com>
2023-05-25 11:33:09 +0900

    feat:리뷰 조회(특정유저)에 식당 이름 추가, 리뷰 내림차순 조회 구현

--- server/src/main/java/application/Review/repository/ReviewRepository.java
+++ server/src/main/java/application/Review/repository/ReviewRepository.java
@@ -14,5 +14,5 @@ public interface ReviewRepository extends JpaRepository<Review, Long>{
 
     List<Review> findAllByMember_MemberId(Long memberId); // 특정 사용자가 작성한 리뷰를 가져오는 방법
 
-    List<Review> findAllByOrderByLikesDesc(); // 모든 리뷰를 좋아요 순으로 내림차순 정렬하여 가져오는 방법
+    List<Review> findAllByOrderByLikeCountDesc(); // 모든 리뷰를 좋아요 순으로 내림차순 정렬하여 가져오는 방법
 }
